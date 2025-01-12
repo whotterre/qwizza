af4cf93599cd5343afcdbaad96df365f1850c6e2
hashkitti001 <iJeddy5.0@gmail.com>
2025-01-12 12:28:37 +0100

    add logic to connect to MySQL db

--- backend/src/server.go
+++ backend/src/server.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"os"
+	"qwizza/utils"
+	"github.com/joho/godotenv"
+)
+
 func main() {
-	
+	godotenv.Load()
+	db_string := os.Getenv("DB_STRING")
+	// Connect to DB
+	utils.ConnectToDB(db_string)
 }
--- backend/src/utils/dbConnect.go
+++ backend/src/utils/dbConnect.go
@@ -1,2 +1,21 @@
 package utils
 
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql" 
+	"fmt"
+	"log"
+)
+// Utility to connect to the database
+func ConnectToDB(db_url string) {
+	
+	db, err := sql.Open("mysql", db_url)
+	if err != nil {
+		log.Fatal("Couldn't connect to database", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Successfully connected to the database")
+}
