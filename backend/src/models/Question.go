a51d70f81623ad3be400196463f8259c5f957d51
hashkitti001 <iJeddy5.0@gmail.com>
2025-01-19 22:01:18 +0100

    initialize restructured branch

--- backend/src/models/Question.go
+++
@@ -1,9 +0,0 @@
-package models
-
-// Data model for a question in a quiz 
-type Question struct {
-	ID            int      `json:"question_id"`  // Unique ID for the question
-	Title         string   `json:"question_title"` // The text of the question
-	CorrectOption string   `json:"correct_option"` // The correct answer for the question
-	Options       []string 	`json:"options"`        // Possible answer options (for multiple choice)
-}
