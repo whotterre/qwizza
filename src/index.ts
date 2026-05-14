2aca7a0606dc8b4653775058cc417eb7db3c26e8
whotterre <ijeddy5.0@gmail.com>
2026-05-14 02:37:52 +0100

    perf: increase max connections on Postgres connection pool

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
 import 'dotenv/config';
 import { drizzle, NodePgDatabase } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
 
-const db: NodePgDatabase = drizzle(process.env.DATABASE_URL!);
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  max: 100,
+  min: 10,
+  idleTimeoutMillis: 30000,
+  ssl: process.env.DATABASE_URL?.includes('localhost') ? false : {
+    rejectUnauthorized: false,
+  },
+});
 
-export default db
+pool.on('error', (err) => {
+  console.error('Unexpected error on idle client', err);
+});
+
+const db: NodePgDatabase = drizzle(pool);
+
+export default db;
