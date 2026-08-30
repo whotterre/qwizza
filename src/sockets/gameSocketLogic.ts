a3a287c60f7644bc3273fffdc0115a08bc03038f
Iwegbu Jedidiah <76275625+whotterre@users.noreply.github.com>
2026-08-30 18:17:50 +0100

    fix(sockets): import WebSocket correctly to resolve TS2702

--- src/sockets/gameSocketLogic.ts
+++ src/sockets/gameSocketLogic.ts
@@ -1,4 +1,4 @@
-import { WebSocket, WebSocketServer } from "ws";
+import WebSocket, { WebSocketServer } from "ws";
 import Redis from "ioredis";
 import GameRepository from "../repositories/game";
 
