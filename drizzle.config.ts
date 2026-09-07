540a380b212b497647749d05a87d18369aa71c7d
Iwegbu Jedidiah <76275625+whotterre@users.noreply.github.com>
2026-09-07 10:01:44 +0100

    Modify drizzle configuration for schema and SSL
    Updated schema path and added SSL configuration.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,18 @@
 import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
+dotenv.config();
+
 export default defineConfig({
-  out: './drizzle',
-  schema: './src/db/schema.ts',
   dialect: 'postgresql',
+  schema: './src/schema.ts',
+  out: './drizzle',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
+    ssl: {
+      rejectUnauthorized: false, 
+    },
   },
+  verbose: true,
+  strict: true,
 });
