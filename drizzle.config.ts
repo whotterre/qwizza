ab5609d77cacb021d0e528a9561881d1e16bc095
Iwegbu Jedidiah <76275625+whotterre@users.noreply.github.com>
2026-09-07 10:12:18 +0100

    Remove SSL settings from database configuration
    Removed SSL configuration from database credentials.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,16 +1,12 @@
 import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
-
 export default defineConfig({
   dialect: 'postgresql',
   schema: './src/db/schema.ts',
   out: './drizzle',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
-    ssl: {
-      rejectUnauthorized: false, 
-    },
   },
   verbose: true,
   strict: true,
