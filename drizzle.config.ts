ca200f9aa39fd66968a6fb6807493eef2520b9c7
Iwegbu Jedidiah <76275625+whotterre@users.noreply.github.com>
2026-09-07 10:08:23 +0100

    Update schema path in drizzle configuration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
   dialect: 'postgresql',
-  schema: './src/schema.ts',
+  schema: './src/db/schema.ts',
   out: './drizzle',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
