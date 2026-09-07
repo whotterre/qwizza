522feaaf383a47ed0383054601891610f977b871
whotterre <ijeddy5.0@gmail.com>
2026-09-07 17:22:54 +0100

    Refactor database credentials in drizzle configuration: extract details from DATABASE_URL

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,20 +1,24 @@
 import { defineConfig } from 'drizzle-kit';
-
 import * as dotenv from "dotenv";
 
 dotenv.config({
   path: ".env",
 });
 
+const dbUrl = new URL(process.env.DATABASE_URL!);
 
 export default defineConfig({
   dialect: 'postgresql',
   schema: './src/db/schema.ts',
   out: './drizzle',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    host: dbUrl.hostname,
+    port: parseInt(dbUrl.port || "5432"),
+    user: dbUrl.username,
+    password: decodeURIComponent(dbUrl.password), 
+    database: dbUrl.pathname.replace(/^\//, ''),
     ssl: {
-       rejectUnauthorized: false,
+       rejectUnauthorized: false, 
     },
   },
   verbose: true,
